9a0f3eeec2e47c4aca11141641d2fe380272fa63
Matthew McGonagle <mwmcgonagle@gmail.com>
2017-03-22 14:06:59 -0700

    Changed to three degree fit for texture3 as a function of texture2 and texture1. Implemented simple nearest point projection. Found a worse logloss of 0.746.

--- analyzeratios.py
+++ analyzeratios.py
@@ -144,67 +144,221 @@ isopscaler = MinMaxScaler()
 train_df['isopratio'] = isopscaler.fit_transform(train_df['isopratio'].values.reshape(-1, 1))
 test_df['isopratio'] = isopscaler.transform(test_df['isopratio'].values.reshape(-1, 1))
 
+# Do Quadratic Fit for Texture Components based on surface.
 
-# Do Quadratic Fit for Texture Components
-
-quadn = {}
-quadn['texture'] = 2
-quadfitcols = ['texture2', 'texture3']
-newquadcols = [name + 'fit' for name in quadfitcols]
-print(newquadcols)
-model = Pipeline([ ('polynomial_features', PolynomialFeatures(2)),
+quadfitX = ['texture1', 'texture2']
+quadfity = ['texture3']
+model = Pipeline([ ('polynomial_features', PolynomialFeatures(3)),
                    ('linear_regression', LinearRegression())
-                 ] )
-model.fit(train_df['texture1'].to_frame(), train_df[quadfitcols] )
-predictresults = model.predict(train_df['texture1'].to_frame())
-for newseries, newvalues in zip(newquadcols, predictresults.T[:]):
-    train_df[newseries] = newvalues
-predictresults = model.predict(test_df['texture1'].to_frame())
-for newseries, newvalues in zip(newquadcols, predictresults.T[:]):
-    test_df[newseries] = newvalues
+                 ])
+model.fit(train_df[quadfitX], train_df[quadfity])
+predictresults = model.predict(train_df[quadfitX])
+for name, newvalues in zip(quadfity, predictresults.T):
+   train_df[name + 'fit'] = newvalues 
+predictresults = model.predict(test_df[quadfitX])
+for name, newvalues in zip(quadfity, predictresults.T):
+   test_df[name + 'fit'] = newvalues 
 
-# Now get info for computing arclength
-print('Linear Regression Coefficients Shape = ', model.named_steps['linear_regression'].coef_.shape)
-quadcoeff = model.named_steps['linear_regression'].coef_.T # Shape is now (n_features, n_targets)
-arclengthparam = np.zeros(3)
-arclengthparam[2] = np.linalg.norm(quadcoeff[2])
-dotAB = np.dot(quadcoeff[2], quadcoeff[1])
-arclengthparam[1] = dotAB * 0.5 / arclengthparam[2]**2
-arclengthparam[0] = 1.0 + np.linalg.norm(quadcoeff[1])**2 - dotAB**2 * arclengthparam[2]**-2.0
-print('Before square root, arclengthparam[0] = ', arclengthparam[0])
-arclengthparam[0] = np.sqrt(arclengthparam[0])
-print('arclengthparam = \n', arclengthparam)
+# Generate grid of fit
+Xfit = np.arange(np.amin(train_df[quadfitX[0]].values) - 0.1,
+                 np.amax(train_df[quadfitX[0]].values) + 0.1,
+                 0.003)
+Yfit = np.arange(np.amin(train_df[quadfitX[1]].values) - 0.1,
+                 np.amax(train_df[quadfitX[1]].values) + 0.1,
+                 0.003)
 
-def antideriv(x): # This is the anti-deriv of sqrt(1 + x**2) which is used to compute arclength
-    result = x * np.sqrt(1 + x**2) + np.arcsinh(x)
-    return result * 0.5
+# Make 3d scatterplot and plot of fit
+varstoplot = quadfitX + quadfity 
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111, projection = '3d' )
+ax2.scatter(train_df[varstoplot[0]], train_df[varstoplot[1]], train_df[varstoplot[2]], c = 'black')
 
-def getarclength(x, arclengthparam):
-    u1 = arclengthparam[1] * 2 * arclengthparam[2] / arclengthparam[0]
-    u2 = u1 + x * 2 * arclengthparam[2] / arclengthparam[0]
-    result = antideriv(u2) - antideriv(u1)
-    result *= arclengthparam[0]**2 * 0.5 / arclengthparam[2]
-    return result
-
-# Transform train_df['texture1'] into arclength along fitted curve
-train_df['texture1'] = train_df['texture1'].apply(lambda x: getarclength(x, arclengthparam) )    
-test_df['texture1'] = test_df['texture1'].apply(lambda x: getarclength(x, arclengthparam) )    
-    
-for name, color in zip(quadfitcols, ['blue', 'black', 'purple']):
-    plt.scatter(train_df['texture1'].values, train_df[name].values, color = color )
-    plt.scatter(train_df['texture1'].values, train_df[name + 'fit'].values, color = 'red')
+Xfit, Yfit = np.meshgrid(Xfit, Yfit)
+Zfit = np.dstack( (Xfit, Yfit) )
+print(Zfit[:3,:4])
+Zfit = [model.predict(x) for x in Zfit[:,:] ]
+Zfit = np.array(Zfit)
+ni, nj, nk = Zfit.shape
+Zfit = Zfit.reshape((ni, nj))
+print(Zfit.shape) 
+print(Xfit.shape)
+ax2.plot_wireframe(Xfit, Yfit, Zfit, rstride = 10, cstride = 10)
 plt.show()
 
-for name in quadfitcols: 
-    train_df[name] = train_df[name] - train_df[name + 'fit']
-    test_df[name] = test_df[name] - test_df[name + 'fit']
+# Now project onto surface
 
+allfit = np.dstack( (Xfit, Yfit, Zfit) )
+print(allfit.shape)
+
+minpos = []
+for point in train_df[quadfitX + quadfity].values[:]:
+    diff = allfit - point
+    diff = np.linalg.norm(diff, axis = 2)
+    newpos = np.unravel_index(np.argmin(diff), diff.shape)
+    minpos.append(newpos)
+print(minpos[0])
+minpos = np.array(minpos)
+projpoints = []
+for i, j in minpos:
+    projpoints.append(allfit[i, j])
+projpoints = np.array(projpoints)
+
+projdiff = train_df[quadfitX + quadfity].values - projpoints
+projerror = []
+for point in projdiff:
+    size = np.linalg.norm(point)
+    if point[2] > 0:
+        projerror.append(size)
+    else:
+        projerror.append(-size)
+
+train_df['texture5'] = projerror
+ncomponents['texture'] = 5
+
+projpoints = projpoints.T
+for i in range(len(projpoints)):
+    train_df['texture' + str(i+1)] = projpoints[i] 
+
+# Now do for test data
+
+minpos = []
+for point in test_df[quadfitX + quadfity].values[:]:
+    diff = allfit - point
+    diff = np.linalg.norm(diff, axis = 2)
+    newpos = np.unravel_index(np.argmin(diff), diff.shape)
+    minpos.append(newpos)
+print(minpos[0])
+minpos = np.array(minpos)
+projpoints = []
+for i, j in minpos:
+    projpoints.append(allfit[i, j])
+projpoints = np.array(projpoints)
+
+projdiff = test_df[quadfitX + quadfity].values - projpoints
+projerror = []
+for point in projdiff:
+    size = np.linalg.norm(point)
+    if point[2] > 0:
+        projerror.append(size)
+    else:
+        projerror.append(-size)
+
+test_df['texture5'] = projerror
+ncomponents['texture'] = 5
+
+
+projpoints = projpoints.T
+print('projpoints shape = ', projpoints.shape)
+for i in range(len(projpoints)):
+    test_df['texture' + str(i+1)] = projpoints[i] 
+
+# This is the quadratic fit based on a curve. Other section contains fit based on surface.
+# # Do Quadratic Fit for Texture Components
+# 
+# quadn = {}
+# quadn['texture'] = 2
+# quadfitcols = ['texture2', 'texture3']
+# newquadcols = [name + 'fit' for name in quadfitcols]
+# print(newquadcols)
+# model = Pipeline([ ('polynomial_features', PolynomialFeatures(2)),
+#                    ('linear_regression', LinearRegression())
+#                  ] )
+# model.fit(train_df['texture1'].to_frame(), train_df[quadfitcols] )
+# predictresults = model.predict(train_df['texture1'].to_frame())
+# for newseries, newvalues in zip(newquadcols, predictresults.T[:]):
+#     train_df[newseries] = newvalues
+# predictresults = model.predict(test_df['texture1'].to_frame())
+# for newseries, newvalues in zip(newquadcols, predictresults.T[:]):
+#     test_df[newseries] = newvalues
+# 
+# for name, color in zip(quadfitcols, ['blue', 'black', 'purple']):
+#     plt.scatter(train_df['texture1'].values, train_df[name].values, color = color )
+#     plt.scatter(train_df['texture1'].values, train_df[name + 'fit'].values, color = 'red')
+# plt.show()
+# 
+# # for name in quadfitcols: 
+# #     train_df[name] = train_df[name] - train_df[name + 'fit']
+# #     test_df[name] = test_df[name] - test_df[name + 'fit']
+# 
+# # Project onto curve
+# dx = 0.01
+# xvalues = np.linspace(-2, 2, num = 400)
+# xvalues = xvalues.reshape(len(xvalues), 1)
+# yzvalues = model.predict(xvalues)
+# allvalues = np.concatenate((xvalues, yzvalues), axis = 1)
+# 
+# minpos = []
+# for point in train_df[['texture1'] + quadfitcols].values:
+#    diff = allvalues - point
+#    diff = np.linalg.norm(diff, axis = 1) 
+#    minpos.append( np.argmin(diff) )
+# minpos = np.array(minpos)
+# 
+# newpos = allvalues[minpos[:]]
+# fitdiff = newpos - train_df[['texture1'] + quadfitcols].values
+# train_df['texture2'] = np.linalg.norm(fitdiff, axis = 1)
+# train_df['texture1'] = newpos.T[0] 
+# # for name, j in zip(quadfitcols, range(1, len(quadfitcols) + 1)):
+# #     train_df[name] = newpos[j] 
+# 
+# minpos = []
+# for point in test_df[['texture1'] + quadfitcols].values:
+#    diff = allvalues - point
+#    diff = np.linalg.norm(diff, axis = 1) 
+#    minpos.append( np.argmin(diff) )
+# minpos = np.array(minpos)
+# 
+# newpos = allvalues[minpos[:]]
+# fitdiff = newpos - test_df[['texture1'] + quadfitcols].values
+# test_df['texture2'] = np.linalg.norm(fitdiff, axis = 1)
+# test_df['texture1'] = newpos.T[0] 
+# # for name, j in zip(quadfitcols, range(1, len(quadfitcols) + 1)):
+# #     train_df[name] = newpos[j] 
+# 
+# # Now get info for computing arclength
+# print('Linear Regression Coefficients Shape = ', model.named_steps['linear_regression'].coef_.shape)
+# quadcoeff = model.named_steps['linear_regression'].coef_.T # Shape is now (n_features, n_targets)
+# arclengthparam = np.zeros(3)
+# arclengthparam[2] = np.linalg.norm(quadcoeff[2])
+# dotAB = np.dot(quadcoeff[2], quadcoeff[1])
+# arclengthparam[1] = dotAB * 0.5 / arclengthparam[2]**2
+# arclengthparam[0] = 1.0 + np.linalg.norm(quadcoeff[1])**2 - dotAB**2 * arclengthparam[2]**-2.0
+# print('Before square root, arclengthparam[0] = ', arclengthparam[0])
+# arclengthparam[0] = np.sqrt(arclengthparam[0])
+# print('arclengthparam = \n', arclengthparam)
+# 
+# def antideriv(x): # This is the anti-deriv of sqrt(1 + x**2) which is used to compute arclength
+#     result = x * np.sqrt(1 + x**2) + np.arcsinh(x)
+#     return result * 0.5
+# 
+# def getarclength(x, arclengthparam):
+#     u1 = arclengthparam[1] * 2 * arclengthparam[2] / arclengthparam[0]
+#     u2 = u1 + x * 2 * arclengthparam[2] / arclengthparam[0]
+#     result = antideriv(u2) - antideriv(u1)
+#     result *= arclengthparam[0]**2 * 0.5 / arclengthparam[2]
+#     return result
+# 
+# # Transform train_df['texture1'] into arclength along fitted curve
+# train_df['texture1'] = train_df['texture1'].apply(lambda x: getarclength(x, arclengthparam) )    
+# test_df['texture1'] = test_df['texture1'].apply(lambda x: getarclength(x, arclengthparam) )    
+#     
+
+# Scale on 'texture1'
 # Need to renormalize texture features using 'texture1'
 collist = getcollist('texture', ncomponents['texture'])
 scaler = MinMaxScaler()
 scaler.fit(train_df['texture1'].to_frame())
 train_df[collist] = scaler.transform(train_df[collist])
 test_df[collist] = scaler.transform(test_df[collist])
+
+# Scale on total size of fitted texture features
+# sizes = train_df[quadfitX + quadfity].values
+# sizes = np.linalg.norm(sizes, axis = 1)
+# scaler = MinMaxScaler()
+# scaler.fit(sizes.reshape((len(sizes), 1)) )
+# collist = getcollist('texture', ncomponents['texture'])
+# train_df[collist] = scaler.transform(train_df[collist])
+# test_df[collist] = scaler.transform(test_df[collist])
 
 # Now setup K Nearest Neighbors Classifier
 
